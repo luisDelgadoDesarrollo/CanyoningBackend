23388a973eb38f1c9be69d2efd32f9952696a10e
Luis Delgado <delgadofernandez.luisdesarrollo@gmail.com>
2024-11-03 16:13:25 +0100

    fix

--- application/src/main/java/es/luis/canyoningApp/application/rest/controller/UserController.java
+++ application/src/main/java/es/luis/canyoningApp/application/rest/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -75,6 +76,7 @@ public class UserController extends BaseController implements UserApi {
     }
 
     @Override
+    @CrossOrigin(origins = "http://localhost:443")
     public ResponseEntity<Void> updatePassword(
             String email, String token, UpdatePasswordRequestDto updatePasswordRequestDto) {
         userService.updatePassword(email, token, updatePasswordRequestDto.getPassword());
