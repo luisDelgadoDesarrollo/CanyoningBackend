d4e46d8e8c1c1244e17d76a2272d4296dd41362f
Luis Delgado <delgadofernandez.luisdesarrollo@gmail.com>
2024-11-04 19:03:27 +0100

    fix def

--- boot/src/main/java/es/luis/canyoningApp/boot/config/boot/WebConfig.java
+++ boot/src/main/java/es/luis/canyoningApp/boot/config/boot/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("https://misbarrancosbdesarrollo.com")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
