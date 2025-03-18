d84ecc2c0fb1d0f752d8ad1181775f6c6ab61560
Luis Delgado <delgadofernandez.luisdesarrollo@gmail.com>
2025-03-18 10:38:03 +0100

    test console

--- boot/src/main/java/es/luis/canyoningApp/boot/config/boot/CustomUserDetailsService.java
+++ boot/src/main/java/es/luis/canyoningApp/boot/config/boot/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package es.luis.canyoningApp.boot.config.boot;
 import es.luis.canyoningApp.domain.model.User;
 import es.luis.canyoningApp.domain.service.UserService;
 import es.luis.canyoningApp.domain.util.MapperUtils;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,16 +11,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log
 public class CustomUserDetailsService implements UserDetailsService {
 
-  @Autowired private UserService userService;
+    @Autowired
+    private UserService userService;
 
-  @Autowired MapperUtils mapperUtils;
+    @Autowired
+    MapperUtils mapperUtils;
 
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-    User userByEmail = userService.getUserByEmail(username);
-    return mapperUtils.userToAuthenticatedUser(userByEmail);
-  }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info(username);
+        User userByEmail = userService.getUserByEmail(username);
+        return mapperUtils.userToAuthenticatedUser(userByEmail);
+    }
 }
