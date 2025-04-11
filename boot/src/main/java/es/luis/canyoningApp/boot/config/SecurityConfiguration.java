9802986812bbf7bb4ba36a9699e851951a16fd82
Luis Delgado <delgadofernandez.luisdesarrollo@gmail.com>
2025-04-11 18:44:07 +0200

    firs commit new version security

--- boot/src/main/java/es/luis/canyoningApp/boot/config/SecurityConfiguration.java
+++ boot/src/main/java/es/luis/canyoningApp/boot/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import es.luis.canyoningApp.domain.security.RoleUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -14,30 +15,46 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity(/*debug = true*/ )
+@EnableWebSecurity(/*debug = true*/)
 public class SecurityConfiguration {
 
-  @Autowired private CustomUserDetailsService userDetailsService;
+    String[] freeGetEnpoints = {
+            "/validateUser",
+            "/canyons",
+            "/canyons/map",
+            "/messages/{activityType}/activity/{activityId}",
+            "images/{image}/{dir}",
+            "articles",
+            "/articles/{articleId}"
+    };
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(
-            (authorizeHttpRequests) ->
-                authorizeHttpRequests
-                    .requestMatchers(
-                        "/login", "/updatePassword", "/createUser", "/", "/validateUser")
-                    .permitAll()
-                    .anyRequest()
-                    .hasAuthority(RoleUtils.ROLE_AUTHENTICATED))
-        .csrf(AbstractHttpConfigurer::disable)
-        .httpBasic(Customizer.withDefaults())
-        .cors(Customizer.withDefaults());
-    return http.build();
-  }
+    String[] freePostEndpoints = {
+            "/createUser", "/updatePassword", "/login",
+    };
 
-  @Bean
-  public AuthenticationManager authenticationManager(
-      AuthenticationConfiguration authenticationConfiguration) throws Exception {
-    return authenticationConfiguration.getAuthenticationManager();
-  }
+    String[] freePutEndpoints = {"/updatePassword"};
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        (authorizeHttpRequests) ->
+                                authorizeHttpRequests
+                                        .requestMatchers(HttpMethod.GET, freeGetEnpoints)
+                                        .permitAll().requestMatchers(HttpMethod.POST, freePostEndpoints).permitAll().requestMatchers(HttpMethod.PUT, freePutEndpoints).permitAll()
+                                        .anyRequest()
+                                        .hasAuthority(RoleUtils.ROLE_AUTHENTICATED))
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .cors(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
